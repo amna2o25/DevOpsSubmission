393c4dbaba76d0e059b06891f04b95cb0edc8623
amna2o25 <s2058660@students.ncl-coll.ac.uk>
2025-05-09 11:38:11 +0100

    test

--- login_process.php
+++ login_process.php
@@ -19,8 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['finalize_checkout']))
 ob_end_flush();  // Send output buffer and turn off output buffering
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
